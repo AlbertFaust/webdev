70d167cbb209f7957abd1af2fd21b33724f6a9c8
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-09-12 21:02:18 +0100

    Updated script.js

--- PerformanceEngineeringLaboratory/script.js
+++ PerformanceEngineeringLaboratory/script.js
@@ -1,6 +1,8 @@
+var clientHeight = document.getElementById('section1').clientHeight;
+//if stylesheet = portrait var = 830
 $('#sidebar').affix({
   offset: {
-    top: 726
+    top: 830
   }
 });
 
@@ -30,3 +32,36 @@ function swapStyleSheet(style){
       i = 0;
     }
 }
+$('.row .thumbnail').on('load', function() {
+
+}).each(function(i) {
+  if(this.complete) {
+  	var item = $('<div class="item"></div>');
+    var itemDiv = $(this).parents('div');
+    var title = $(this).parent('a').attr("title");
+
+    item.attr("title",title);
+  	$(itemDiv.html()).appendTo(item);
+  	item.appendTo('.carousel-inner');
+    if (i==0){ // set first item active
+     item.addClass('active');
+    }
+  }
+});
+
+/* activate the carousel */
+$('#modalCarousel').carousel({interval:false});
+
+/* change modal title when slide changes */
+$('#modalCarousel').on('slid.bs.carousel', function () {
+  $('.modal-title').html($(this).find('.active').attr("title"));
+})
+
+/* when clicking a thumbnail */
+$('.row .thumbnail').click(function(){
+    var idx = $(this).parents('div').index();
+  	var id = parseInt(idx);
+  	$('#myModal').modal('show'); // show the modal
+    $('#modalCarousel').carousel(id); // slide carousel to selected
+
+});
